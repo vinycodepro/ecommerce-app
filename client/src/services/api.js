833644be62c0287703680cafc32b7cd0ebf3762d
vinycodepro <vincentmunyanya197@gmail.com>
2026-03-19 10:53:15 +0300

    refactor: simplify API_URL configuration to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,7 @@
 // client/src/services/api.js
 import axios from 'axios';
 
-const API_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "https://ecommerce-app-1-pxaw.onrender.com/api";
-
-
+ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 
 const api = axios.create({
   baseURL: API_URL,
